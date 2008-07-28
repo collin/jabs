ce4e347fc09b234824a250545b309825c0b44df7
collin <collintmiller@gmail.com>
2008-07-28 01:48:34 -0500

    re tooled to steal ideas from jabl, now compiling examples

--- examples/input_with_default.js
+++ examples/input_with_default.js
@@ -1,17 +1,15 @@
 jQuery(function() {
-  jQuery("input[default_value]").each(function() {
-    var value = jQuery(this).attr("default_value");
-    jQuery(this).bind("blur", function() {
-      console.log(jQuery(this).val() === "");
-      if(jQuery(this).val() === "") {
-        jQuery(this).val(value);
+  (function($this) {
+    $this.bind("blur", function() {
+      if($this.attr("value") === "") {
+        $this.attr("value", $this.attr("default_value"));
 }
 });
-    jQuery(this).bind("focus", function() {
-      if(jQuery(this).val() === value) {
-        jQuery(this).val("");
+    $this.bind("focus", function() {
+      if($this.attr("value") === $this.attr("default_value")) {
+        $this.attr("value", "");
 }
 });
-    jQuery(this).blur();
-});
+    $this.blur();
+})(jQuery("input[default_value]"));
 });
